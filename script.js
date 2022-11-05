eec484ee3ecfcdb24fed592803a61c7a49c6127d
Ahmad Aldihani <aldihani4336@gmail.com>
2022-11-05 19:47:41 +0300

    done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // أنشئ المصفوفة تحت هذا الكومنت
-
+let orders = []
 
 function addOrder() {
   let name = document.getElementById("name").value;
@@ -9,15 +9,26 @@ function addOrder() {
   
   // أنشئ كائن newOrder
   
+let newOrder = { name, id, phone, order}
   // أدرج الكائن إلى المصفوفة
-  
+  orders.push(newOrder)
   // طبق الخطوة 7 تحت هذا الكومنت
-  
+  load()  
 }
 
 function load() {
   let container = document.getElementById("container");
   container.innerHTML = "";
   // طبّق الخطوة 6 تحت هذا الكومنت
+  orders.forEach((item,index) => {
+    container.innerHTML += `<div><h1>${item.name} - ${item.id}</h1><h3>${item.order}</h3> <button onclick="del()">delete</button></div>`
 
+    
+});
+}
+
+
+function del() {
+  orders.pop()
+  load()
 }
